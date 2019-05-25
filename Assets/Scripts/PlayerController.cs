5f64338fb32827f321684024d6f969479973ffd2
Uriel Huertas <pryonx8@gmail.com>
2019-05-25 16:47:06 +0200

    Copied movement and controller from my test game project.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   [Range(0, .3f)] 
+   [SerializeField] 
+   private float m_MovementSmoothing = .05f;
+
+   private Rigidbody2D m_Rigidbody2D;
+   private bool m_FacingRight = true;
+   private Vector3 m_Velocity = Vector3.zero;
+
+   private void Awake()
+   {
+      m_Rigidbody2D = GetComponent<Rigidbody2D>();
+   }
+
+   public void Move(float move)
+   {
+
+      Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+ 
+      m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+ 
+      if (move > 0 && !m_FacingRight)
+      {
+         Flip();
+      } else if (move < 0 && m_FacingRight)
+      {
+         Flip();
+      }
+        
+        
+   }
+
+
+   private void Flip()
+   {
+      m_FacingRight = !m_FacingRight;
+
+      Vector3 theScale = transform.localScale;
+      theScale.x *= -1;
+      transform.localScale = theScale;
+   }
+   
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,17 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
+    public PlayerController controller;
 
-    private void Update()
+    private float translation = 0f;
+
+    void Update()
     {
-        var translation = Input.GetAxis("Horizontal") * speed;
+        translation = Input.GetAxis("Horizontal") * speed;
+    }
 
-        translation *= Time.deltaTime;
-        
-        transform.Translate(translation, 0, 0);
+    private void FixedUpdate()
+    {
+        controller.Move(translation * Time.deltaTime);
     }
 }
